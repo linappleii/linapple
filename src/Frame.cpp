fa2b95c3ee43ec72b0c22061acf0000b8c1ea6bd
Chris Pressey <cpressey@catseye.tc>
2014-08-28 13:39:35 +0100

    Modern g++ needs unistd.h to be #include'd explicitly.

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -38,6 +38,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 // for stat in FrameSaveBMP function
 #include <sys/stat.h>
 
+// for usleep()
+#include <unistd.h>
+
 
 #define ENABLE_MENU 0
 
