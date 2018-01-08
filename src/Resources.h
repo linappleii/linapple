91058c4fd8032f97f0f1d8c1b043de4515457789
Chris Pressey <cpressey@catseye.tc>
2018-01-08 16:38:32 +0000

    Describe the file and provide a default RESOURCES_DIR if not given.

--- src/Resources.h
+++ src/Resources.h
@@ -1,3 +1,9 @@
+/* Resources.h -- configures locations of resource files that linapple loads on startup */
+
+#ifndef RESOURCE_DIR
+  #define RESOURCE_DIR "./"
+#endif
+
 #define RESOURCE_ICON_BMP       RESOURCE_DIR "icon.bmp"
 #define RESOURCE_SPLASH_BMP     RESOURCE_DIR "splash.bmp"
 #define RESOURCE_CHARSET40_BMP  RESOURCE_DIR "charset40.bmp"
