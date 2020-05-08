38351d727d49ec1cf6484d40b49c746e6adac97e
Ludovic <ludovic@hirlimann.net>
2020-05-08 15:11:27 +0200

    give the --conf switch so people are aware when they type -h

--- src/Applewin.cpp
+++ src/Applewin.cpp
@@ -988,6 +988,7 @@ void PrintHelp()
          "  -r PAGES       allocate PAGES to Ramworks (1-127)\n"
          #endif
          "  --benchmark    benchmark and quit\n"
+         "  --conf <file>  conf file for the emulator\n"
          "\n");
 }
 
