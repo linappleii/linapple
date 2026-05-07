e9299c51f49385aea8b6b00bff123994e38ef216
David Baucum <david@baucum.me>
2026-05-07 01:05:52 -0400

    Finalize Clock peripheral refactor with quality compliance

--- src/apple2/Clock.cpp
+++ src/apple2/Clock.cpp
@@ -1,31 +1,38 @@
-#include "core/Common.h"
-#include <cstddef>
-#include <ctime>
-#include <cstdio>
-#include <cassert>
-#include <cstring>
-#include <cstdint>
-#include <array>
 #include "apple2/Clock.h"
+
+// NOLINTBEGIN(bugprone-easily-swappable-parameters,
+// modernize-use-trailing-return-type, cppcoreguidelines-owning-memory,
+// cppcoreguidelines-avoid-non-const-global-variables,
+// cppcoreguidelines-avoid-magic-numbers, cppcoreguidelines-avoid-c-arrays,
+// modernize-avoid-c-arrays, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+
+#include <array>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+
 #include "apple2/Memory.h"
 #include "apple2/Structs.h"
+#include "core/Common.h"
 #include "core/Peripheral.h"
-
 
 /*
 I/O map: (please add an offset of Slot#*16 to the address below)
 
         C080	(r)   latched month (tens)
-	C081	(r)   latched month (units)
+        C081	(r)   latched month (units)
         C082	(r)   always zero
-	C083	(r)   latched day-of-week
+        C083	(r)   latched day-of-week
         C084	(r)   latched day-of-month (tens)
-	C085	(r)   latched day-of-month (units)
+        C085	(r)   latched day-of-month (units)
         C086	(r)   latched hour (tens)
-	C087	(r)   latched hour (units)
+        C087	(r)   latched hour (units)
         C088	(r)   latched minute (tens)
-	C089	(r)   latched minute (units)
-	C08F	(r)   get system clock and update the latched values
+        C089	(r)   latched minute (units)
+        C08F	(r)   get system clock and update the latched values
 */
 
 /*
@@ -62,101 +69,101 @@ I/O map: (please add an offset of Slot#*16 to the address below)
   it is processed.
 */
 
-
 static const std::array<uint8_t, 95> Clock_ROM =
-/*
-*
-* ROM code for a simplistic ProDOS-compatible clock card
-* for the Apple II emulator 'linapple'
-*
-* (C) 2008 by LEE Sau Dan
-*
- ORG $C000
-STACK EQU $100
-IN EQU $200
-DEVSEL EQU $C080
-MONRTS EQU $FF58
-LATCHIT EQU DEVSEL+$F
+    /*
+    *
+    * ROM code for a simplistic ProDOS-compatible clock card
+    * for the Apple II emulator 'linapple'
+    *
+    * (C) 2008 by LEE Sau Dan
+    *
+     ORG $C000
+    STACK EQU $100
+    IN EQU $200
+    DEVSEL EQU $C080
+    MONRTS EQU $FF58
+    LATCHIT EQU DEVSEL+$F
 
-ST
- PHP  ; this opcode byte must be $08 for prodos to recognize
- BCC D1 ; offset byte must be $28 for prodos to recognize
-B1
- BCS D2 ; offset byte must be $58 for prodos to recognize
-B2
- DB 00
- DB $70 ; byte must be $70 for prodos to recognize
- DB 00
-RDCLK ; ProDOS calls $Cx08 to get clock data
- NOP
- NOP
- DB $A9 ; opcode for LDA #xx, just to skip the next byte
-WRCLK ; ProDOS calls $Cx0B with AX="#" before calling RDCLK
- RTS
+    ST
+     PHP  ; this opcode byte must be $08 for prodos to recognize
+     BCC D1 ; offset byte must be $28 for prodos to recognize
+    B1
+     BCS D2 ; offset byte must be $58 for prodos to recognize
+    B2
+     DB 00
+     DB $70 ; byte must be $70 for prodos to recognize
+     DB 00
+    RDCLK ; ProDOS calls $Cx08 to get clock data
+     NOP
+     NOP
+     DB $A9 ; opcode for LDA #xx, just to skip the next byte
+    WRCLK ; ProDOS calls $Cx0B with AX="#" before calling RDCLK
+     RTS
 
-RDCLK1
- PHP
- SEI
- JSR MONRTS ; known to be RTS
- TSX
- LDA STACK,X ; high byte of PC
- PLP
- ASL
- ASL
- ASL
- ASL
- TAY
+    RDCLK1
+     PHP
+     SEI
+     JSR MONRTS ; known to be RTS
+     TSX
+     LDA STACK,X ; high byte of PC
+     PLP
+     ASL
+     ASL
+     ASL
+     ASL
+     TAY
 
- LDA LATCHIT,Y ; latch current time to regs
- LDX #$0
- BEQ RDCLK1_CONT
-
-
- DS B1+$28-*
-D1
- PLP
- RTS
+     LDA LATCHIT,Y ; latch current time to regs
+     LDX #$0
+     BEQ RDCLK1_CONT
 
 
-RDCLK1_CONT
- LDA DEVSEL,Y
- INY
- ORA #"0"
- STA IN,X
- INX
- LDA DEVSEL,Y
- INY
- ORA #"0"
- STA IN,X
- INX
- LDA #","
- STA IN,X
- INX
- TYA
- AND #$0F
- CMP #10
- BCC RDCLK1_CONT
- LDA #$80 ; EOS
- STA IN-1,X ; overwrite the last ','
- RTS
+     DS B1+$28-*
+    D1
+     PLP
+     RTS
 
 
- DS B2+$58-*
-D2
- BCS D1
+    RDCLK1_CONT
+     LDA DEVSEL,Y
+     INY
+     ORA #"0"
+     STA IN,X
+     INX
+     LDA DEVSEL,Y
+     INY
+     ORA #"0"
+     STA IN,X
+     INX
+     LDA #","
+     STA IN,X
+     INX
+     TYA
+     AND #$0F
+     CMP #10
+     BCC RDCLK1_CONT
+     LDA #$80 ; EOS
+     STA IN-1,X ; overwrite the last ','
+     RTS
 
 
- END
-*/
-  {{
-    0x08, 0x90, 0x28, 0xb0, 0x58, 0x00, 0x70, 0x00, 0xea, 0xea, 0xa9, 0x60, 0x08, 0x78, 0x20, 0x58,
-    0xff, 0xba, 0xbd, 0x00, 0x01, 0x28, 0x0a, 0x0a, 0x0a, 0x0a, 0xa8, 0xb9, 0x8f, 0xc0, 0xa2, 0x00,
-    0xf0, 0x0b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x28, 0x60, 0xb9, 0x80, 0xc0,
-    0xc8, 0x09, 0xb0, 0x9d, 0x00, 0x02, 0xe8, 0xb9, 0x80, 0xc0, 0xc8, 0x09, 0xb0, 0x9d, 0x00, 0x02,
-    0xe8, 0xa9, 0xac, 0x9d, 0x00, 0x02, 0xe8, 0x98, 0x29, 0x0f, 0xc9, 0x0a, 0x90, 0xdf, 0xa9, 0x80,
-    0x9d, 0xff, 0x01, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb0, 0xcc,
-  }};
+     DS B2+$58-*
+    D2
+     BCS D1
 
+
+     END
+    */
+    {{
+        0x08, 0x90, 0x28, 0xb0, 0x58, 0x00, 0x70, 0x00, 0xea, 0xea, 0xa9, 0x60,
+        0x08, 0x78, 0x20, 0x58, 0xff, 0xba, 0xbd, 0x00, 0x01, 0x28, 0x0a, 0x0a,
+        0x0a, 0x0a, 0xa8, 0xb9, 0x8f, 0xc0, 0xa2, 0x00, 0xf0, 0x0b, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x28, 0x60, 0xb9, 0x80, 0xc0,
+        0xc8, 0x09, 0xb0, 0x9d, 0x00, 0x02, 0xe8, 0xb9, 0x80, 0xc0, 0xc8, 0x09,
+        0xb0, 0x9d, 0x00, 0x02, 0xe8, 0xa9, 0xac, 0x9d, 0x00, 0x02, 0xe8, 0x98,
+        0x29, 0x0f, 0xc9, 0x0a, 0x90, 0xdf, 0xa9, 0x80, 0x9d, 0xff, 0x01, 0x60,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb0, 0xcc,
+    }};
 
 struct ClockPeripheral_t {
   std::array<uint8_t, 10> latches{};
@@ -167,12 +174,13 @@ struct ClockPeripheral_t {
 static void set_latch_pair(ClockPeripheral_t* cp, int index, int value) {
   cp->latches[static_cast<size_t>(index &= 0x0E)] =
       static_cast<uint8_t>((value %= 100) / 10);
-  cp->latches[static_cast<size_t>(index | 1)] = static_cast<uint8_t>(value % 10);
+  cp->latches[static_cast<size_t>(index | 1)] =
+      static_cast<uint8_t>(value % 10);
 }
 
 static void update_latches(ClockPeripheral_t* cp) {
   time_t t = 0;
-  struct tm tm {};
+  struct tm tm{};
 
   time(&t);
   localtime_r(&t, &tm);
@@ -304,15 +312,20 @@ Peripheral_t g_clock_peripheral = {
     Clock_ABI_Init,
     Clock_ABI_Reset,
     Clock_ABI_Shutdown,
-    nullptr, // think
-    nullptr, // on_vblank
+    nullptr,  // think
+    nullptr,  // on_vblank
     Clock_ABI_SaveState,
     Clock_ABI_LoadState,
-    nullptr, // command
-    nullptr  // query
+    nullptr,  // command
+    nullptr   // query
 };
-
 
 #ifdef BUILD_SHARED_PERIPHERAL
 EXPORT_PERIPHERAL(g_clock_peripheral)
 #endif
+
+// NOLINTEND(bugprone-easily-swappable-parameters,
+// modernize-use-trailing-return-type, cppcoreguidelines-owning-memory,
+// cppcoreguidelines-avoid-non-const-global-variables,
+// cppcoreguidelines-avoid-magic-numbers, cppcoreguidelines-avoid-c-arrays,
+// modernize-avoid-c-arrays, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
--- tests/test_clock_peripheral.cpp
+++ tests/test_clock_peripheral.cpp
@@ -1,140 +1,170 @@
-#include "doctest.h"
-#include "core/Peripheral.h"
-#include "core/Common_Globals.h"
+// NOLINTBEGIN(bugprone-easily-swappable-parameters,
+// modernize-use-trailing-return-type, cppcoreguidelines-owning-memory,
+// cppcoreguidelines-avoid-non-const-global-variables,
+// cppcoreguidelines-avoid-magic-numbers, cppcoreguidelines-avoid-c-arrays,
+// modernize-avoid-c-arrays, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+
+#include <array>
 #include <cstring>
 #include <map>
-#include <array>
 #include <vector>
+
+#include "core/Common_Globals.h"
+#include "core/Peripheral.h"
+#include "doctest.h"
 
 extern Peripheral_t g_clock_peripheral;
 
 // Mock host interface for testing
 struct MockHandler {
-    void* instance;
-    PeripheralIOHandler read;
-    PeripheralIOHandler write;
+  void* instance;
+  PeripheralIOHandler read;
+  PeripheralIOHandler write;
 };
 
 static std::map<uint16_t, MockHandler> g_mock_handlers;
 
-static void Mock_Log(void* instance, PeripheralLogLevel level, const char* fmt, ...) {
-    (void)instance; (void)level; (void)fmt;
+static void Mock_Log(void* instance, PeripheralLogLevel level, const char* fmt,
+                     ...) {
+  (void)instance;
+  (void)level;
+  (void)fmt;
 }
 
 static void Mock_AssertIrq(int slot, bool assert) {
-    (void)slot; (void)assert;
+  (void)slot;
+  (void)assert;
 }
 
-static void Mock_RegisterIO(int slot, PeripheralIOHandler readC0, PeripheralIOHandler writeC0,
-                             PeripheralIOHandler readCx, PeripheralIOHandler writeCx) {
-    (void)slot; (void)readC0; (void)writeC0; (void)readCx; (void)writeCx;
+static void Mock_RegisterIO(int slot, PeripheralIOHandler readC0,
+                            PeripheralIOHandler writeC0,
+                            PeripheralIOHandler readCx,
+                            PeripheralIOHandler writeCx) {
+  (void)slot;
+  (void)readC0;
+  (void)writeC0;
+  (void)readCx;
+  (void)writeCx;
 }
 
 static void Mock_RegisterCxROM(int slot, uint8_t* rom_ptr) {
-    (void)slot; (void)rom_ptr;
+  (void)slot;
+  (void)rom_ptr;
 }
 
 static void Mock_RegisterExpansionROM(int slot, uint8_t* rom_ptr) {
-    (void)slot; (void)rom_ptr;
+  (void)slot;
+  (void)rom_ptr;
 }
 
-static void Mock_RegisterDirectIO(void* instance, uint16_t addr, PeripheralIOHandler read, PeripheralIOHandler write) {
-    g_mock_handlers[addr] = {instance, read, write};
+static void Mock_RegisterDirectIO(void* instance, uint16_t addr,
+                                  PeripheralIOHandler read,
+                                  PeripheralIOHandler write) {
+  g_mock_handlers[addr] = {instance, read, write};
 }
 
 static HostInterface_t mock_host = {
-    /*.Log =*/ Mock_Log,
-    /*.AssertIrq =*/ Mock_AssertIrq,
-    /*.RegisterIO =*/ Mock_RegisterIO,
-    /*.RegisterCxROM =*/ Mock_RegisterCxROM,
-    /*.RegisterExpansionROM =*/ Mock_RegisterExpansionROM,
-    /*.RegisterDirectIO =*/ Mock_RegisterDirectIO,
-    /*.GetMemPtr =*/ nullptr,
-    /*.GetCycles =*/ nullptr,
-    /*.GetConfig =*/ nullptr,
-    /*.SetConfig =*/ nullptr,
-    /*.NotifyStatusChanged =*/ nullptr,
-    /*.NotifyActivityChanged =*/ nullptr,
-    /*.RequestPreciseTiming =*/ nullptr,
-    /*.RiffInitWriteFile =*/ nullptr,
-    /*.RiffFinishWriteFile =*/ nullptr,
-    /*.RiffPutSamples =*/ nullptr,
-    /*.AudioPushSamples =*/ nullptr,
-    /*.ResetSystem =*/ nullptr
-};
+    /*.Log =*/Mock_Log,
+    /*.AssertIrq =*/Mock_AssertIrq,
+    /*.RegisterIO =*/Mock_RegisterIO,
+    /*.RegisterCxROM =*/Mock_RegisterCxROM,
+    /*.RegisterExpansionROM =*/Mock_RegisterExpansionROM,
+    /*.RegisterDirectIO =*/Mock_RegisterDirectIO,
+    /*.GetMemPtr =*/nullptr,
+    /*.GetCycles =*/nullptr,
+    /*.GetConfig =*/nullptr,
+    /*.SetConfig =*/nullptr,
+    /*.NotifyStatusChanged =*/nullptr,
+    /*.NotifyActivityChanged =*/nullptr,
+    /*.RequestPreciseTiming =*/nullptr,
+    /*.RiffInitWriteFile =*/nullptr,
+    /*.RiffFinishWriteFile =*/nullptr,
+    /*.RiffPutSamples =*/nullptr,
+    /*.AudioPushSamples =*/nullptr,
+    /*.ResetSystem =*/nullptr};
 
 TEST_CASE("Clock Peripheral: Lifecycle and I/O Registration") {
-    g_mock_handlers.clear();
-    void* instance = g_clock_peripheral.init(0, &mock_host);
-    REQUIRE(instance != nullptr);
+  g_mock_handlers.clear();
+  void* instance = g_clock_peripheral.init(0, &mock_host);
+  REQUIRE(instance != nullptr);
 
-    // Verify $C080-$C08F are registered
-    for (uint16_t addr = 0xC080; addr <= 0xC08F; ++addr) {
-        CHECK(g_mock_handlers.count(addr) > 0);
-        CHECK(g_mock_handlers[addr].read != nullptr);
-    }
+  // Verify $C080-$C08F are registered
+  for (uint16_t addr = 0xC080; addr <= 0xC08F; ++addr) {
+    CHECK(g_mock_handlers.count(addr) > 0);
+    CHECK(g_mock_handlers[addr].read != nullptr);
+  }
 
-    g_clock_peripheral.shutdown(instance);
+  g_clock_peripheral.shutdown(instance);
 }
 
 TEST_CASE("Clock Peripheral: Time Latching Behavior") {
-    g_mock_handlers.clear();
-    void* instance = g_clock_peripheral.init(0, &mock_host);
+  g_mock_handlers.clear();
+  void* instance = g_clock_peripheral.init(0, &mock_host);
 
-    // 1. Trigger latch update by reading $C08F
-    g_mock_handlers[0xC08F].read(instance, 0, 0xC08F, 0, 0, 0);
+  // 1. Trigger latch update by reading $C08F
+  g_mock_handlers[0xC08F].read(instance, 0, 0xC08F, 0, 0, 0);
 
-    // 2. Read latches $C080-$C089
-    // ProDOS clock latches are BCD: MM W DD HH MM (two digits each)
-    // We can't easily predict the exact time, but we can verify they are valid BCD (0-9)
-    for (uint16_t addr = 0xC080; addr <= 0xC089; ++addr) {
-        uint8_t val = g_mock_handlers[addr].read(instance, 0, addr, 0, 0, 0);
-        CHECK(val <= 9);
-    }
+  // 2. Read latches $C080-$C089
+  // ProDOS clock latches are BCD: MM W DD HH MM (two digits each)
+  // We can't easily predict the exact time, but we can verify they are valid
+  // BCD (0-9)
+  for (uint16_t addr = 0xC080; addr <= 0xC089; ++addr) {
+    uint8_t val = g_mock_handlers[addr].read(instance, 0, addr, 0, 0, 0);
+    CHECK(val <= 9);
+  }
 
-    g_clock_peripheral.shutdown(instance);
+  g_clock_peripheral.shutdown(instance);
 }
 
 TEST_CASE("Clock Peripheral: State Persistence") {
-    g_mock_handlers.clear();
-    void* instance1 = g_clock_peripheral.init(0, &mock_host);
+  g_mock_handlers.clear();
+  void* instance1 = g_clock_peripheral.init(0, &mock_host);
 
-    // 1. Latch a time
-    g_mock_handlers[0xC08F].read(instance1, 0, 0xC08F, 0, 0, 0);
+  // 1. Latch a time
+  g_mock_handlers[0xC08F].read(instance1, 0, 0xC08F, 0, 0, 0);
 
-    // 2. Capture latched values
-    std::array<uint8_t, 10> latched_values;
-    for (uint16_t i = 0; i < 10; ++i) {
-        latched_values[i] = g_mock_handlers[0xC080 + i].read(instance1, 0, 0xC080 + i, 0, 0, 0);
-    }
+  // 2. Capture latched values
+  std::array<uint8_t, 10> latched_values;
+  for (uint16_t i = 0; i < 10; ++i) {
+    latched_values[i] =
+        g_mock_handlers[0xC080 + i].read(instance1, 0, 0xC080 + i, 0, 0, 0);
+  }
 
-    // 3. Save state
-    size_t state_size = 0;
-    g_clock_peripheral.save_state(instance1, nullptr, &state_size);
-    REQUIRE(state_size > 0);
+  // 3. Save state
+  size_t state_size = 0;
+  g_clock_peripheral.save_state(instance1, nullptr, &state_size);
+  REQUIRE(state_size > 0);
 
-    std::vector<uint8_t> buffer(state_size);
-    g_clock_peripheral.save_state(instance1, buffer.data(), &state_size);
+  std::vector<uint8_t> buffer(state_size);
+  g_clock_peripheral.save_state(instance1, buffer.data(), &state_size);
 
-    // 4. Create a new instance and load state
-    void* instance2 = g_clock_peripheral.init(0, &mock_host);
+  // 4. Create a new instance and load state
+  void* instance2 = g_clock_peripheral.init(0, &mock_host);
 
-    // Initially latches should be different or zeroed (Reset zeroes them)
-    g_clock_peripheral.reset(instance2);
-    for (uint16_t i = 0; i < 10; ++i) {
-        CHECK(g_mock_handlers[0xC080 + i].read(instance2, 0, 0xC080 + i, 0, 0, 0) == 0);
-    }
+  // Initially latches should be different or zeroed (Reset zeroes them)
+  g_clock_peripheral.reset(instance2);
+  for (uint16_t i = 0; i < 10; ++i) {
+    CHECK(g_mock_handlers[0xC080 + i].read(instance2, 0, 0xC080 + i, 0, 0, 0) ==
+          0);
+  }
 
-    // Load the saved state
-    PeripheralStatus status = g_clock_peripheral.load_state(instance2, buffer.data(), state_size);
-    CHECK(status == PERIPHERAL_OK);
+  // Load the saved state
+  PeripheralStatus status =
+      g_clock_peripheral.load_state(instance2, buffer.data(), state_size);
+  CHECK(status == PERIPHERAL_OK);
 
-    // 5. Verify restored values match original latched values
-    for (uint16_t i = 0; i < 10; ++i) {
-        uint8_t restored = g_mock_handlers[0xC080 + i].read(instance2, 0, 0xC080 + i, 0, 0, 0);
-        CHECK(restored == latched_values[i]);
-    }
+  // 5. Verify restored values match original latched values
+  for (uint16_t i = 0; i < 10; ++i) {
+    uint8_t restored =
+        g_mock_handlers[0xC080 + i].read(instance2, 0, 0xC080 + i, 0, 0, 0);
+    CHECK(restored == latched_values[i]);
+  }
 
-    g_clock_peripheral.shutdown(instance1);
-    g_clock_peripheral.shutdown(instance2);
+  g_clock_peripheral.shutdown(instance1);
+  g_clock_peripheral.shutdown(instance2);
 }
+// NOLINTEND(bugprone-easily-swappable-parameters,
+// modernize-use-trailing-return-type, cppcoreguidelines-owning-memory,
+// cppcoreguidelines-avoid-non-const-global-variables,
+// cppcoreguidelines-avoid-magic-numbers, cppcoreguidelines-avoid-c-arrays,
+// modernize-avoid-c-arrays, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
