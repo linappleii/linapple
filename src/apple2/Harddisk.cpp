7a6fe5d5a5b122753c1dd069094cfce7de32390a
David Baucum <david@baucum.me>
2026-05-08 20:20:12 -0400

    Encapsulate Harddisk state into HarddiskPeripheral_t structure

--- src/apple2/Harddisk.cpp
+++ src/apple2/Harddisk.cpp
@@ -1,3 +1,29 @@
+/*
+linapple : An Apple //e emulator for Linux
+
+Copyright (C) 1994-1996, Michael O'Brien
+Copyright (C) 1999-2001, Oliver Schmidt
+Copyright (C) 2002-2005, Tom Charlesworth
+Copyright (C) 2006-2007, Tom Charlesworth, Michael Pohoreski
+
+AppleWin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+AppleWin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AppleWin; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* Description: Harddisk hardware emulation (Core)
+ */
+
 #include "apple2/Harddisk.h"
 
 #include <sys/stat.h>
@@ -16,23 +42,26 @@
 #include "core/Common_Globals.h"
 #include "core/LinAppleCore.h"
 #include "core/Log.h"
+#include "core/Peripheral.h"
 #include "core/Registry.h"
 #include "core/Util_Path.h"
 #include "core/Util_Text.h"
+#include "apple2/HarddiskFormatDriver.h"
+#include "apple2/HarddiskLoader.h"
 
 extern void FrameRefreshStatus(int);
 
-char Hddrvr_dat[] =
+static const char Hddrvr_dat[] =
     "\xA9\x20\xA9\x00\xA9\x03\xA9\x3C\xA9\x00\x8D\xF2\xC0\xA9\x70\x8D"
     "\xF3\xC0\xAD\xF0\xC0\x48\xAD\xF1\xC0\x18\xC9\x01\xD0\x01\x38\x68"
     "\x90\x03\x4C\x00\xC6\xA9\x70\x85\x43\xA9\x00\x85\x44\x85\x46\x85"
     "\x47\xA9\x08\x85\x45\xA9\x01\x85\x42\x20\x46\xC7\x90\x03\x4C\x00"
     "\xC6\xA2\x70\x4C\x01\x08\x18\xA5\x42\x8D\xF2\xC0\xA5\x43\x8D\xF3"
-    "\xC0\xA5\x44\x8D\xF4\xC0\xA5\x45\x8D\xF5\xC0\xA5\x46\x8D\xF6\xC0"
-    "\xA5\x47\x8D\xF7\xC0\xAD\xF0\xC0\x48\xA5\x42\xC9\x01\xD0\x03\x20"
-    "\x7D\xC7\xAD\xF1\xC0\x18\xC9\x01\xD0\x01\x38\x68\x60\x98\x48\xA0"
-    "\x00\xAD\xF8\xC0\x91\x44\xC8\xD0\xF8\xE6\x45\xA0\x00\xAD\xF8\xC0"
-    "\x91\x44\xC8\xD0\xF8\x68\xA8\x60\x00\x00\x00\x00\x00\x00\x00\x00"
+    "\xC0\xA5\x44\x8D\xF4\xC0\xA5\x45\x8D\xF6\xC0\xA5\x47\x8D\xF7\xC0"
+    "\xAD\xF0\xC0\x48\xA5\x42\xC9\x01\xD0\x03\x20\x7D\xC7\xAD\xF1\xC0"
+    "\x18\xC9\x01\xD0\x01\x38\x68\x60\x98\x48\xA0\x00\xAD\xF8\xC0"
+    "\x91\x44\xC8\xD0\xF8\xE6\x45\xA0\x00\xAD\xF8\xC0\x91\x44\xC8\xD0"
+    "\xF8\x68\xA8\x60\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -40,7 +69,7 @@ char Hddrvr_dat[] =
     "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF\x7F\xD7\x46";
 
-using HDD = struct {
+struct HarddiskDrive_t {
   char hd_imagename[16];
   char hd_fullname[128];
   uint8_t hd_error;
@@ -48,31 +77,62 @@ using HDD = struct {
   uint16_t hd_diskblock;
   uint16_t hd_buf_ptr;
   bool hd_imageloaded;
-  FilePtr hd_file;
+  FilePtr hd_file; // Keep for now until Phase 3 (Raw driver extraction)
   uint8_t hd_buf[513];
+
+  HarddiskDrive_t()
+      : hd_error(0),
+        hd_memblock(0),
+        hd_diskblock(0),
+        hd_buf_ptr(0),
+        hd_imageloaded(false),
+        hd_file(nullptr, fclose) {
+    memset(hd_imagename, 0, sizeof(hd_imagename));
+    memset(hd_fullname, 0, sizeof(hd_fullname));
+    memset(hd_buf, 0, sizeof(hd_buf));
+  }
 };
-using PHDD = HDD*;
 
-static bool g_bHD_RomLoaded = false;
-bool g_bHD_Enabled = false;
+struct HarddiskPeripheral_t {
+  HarddiskDrive_t drives[2];
+  uint8_t unit_num;
+  uint8_t command;
+  bool rom_loaded;
+  bool enabled;
+  uint32_t slot;
+  int status;
+  HostInterface_t* host;
 
-static uint8_t g_nHD_UnitNum = DRIVE_1;
-static uint8_t g_nHD_Command;
-static HDD g_HardDrive[2] = {
-    {{0}, {0}, 0, 0, 0, 0, false, FilePtr(nullptr, fclose), {0}},
-    {{0}, {0}, 0, 0, 0, 0, false, FilePtr(nullptr, fclose), {0}}};
-static uint32_t g_uSlot = 7;
-static int HDDStatus = DISK_STATUS_OFF;
+  HarddiskPeripheral_t()
+      : unit_num(DRIVE_1),
+        command(0),
+        rom_loaded(false),
+        enabled(false),
+        slot(7),
+        status(DISK_STATUS_OFF),
+        host(nullptr) {}
+};
 
-auto HD_GetStatus() -> int { return HDDStatus; }
-void HD_ResetStatus() { HDDStatus = DISK_STATUS_OFF; }
+// NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables)
+static HarddiskPeripheral_t* g_pHDInstance = nullptr;
+// NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
 
-static void GetImageTitle(const char* imageFileName, PHDD pHardDrive) {
+auto HD_GetStatus() -> int {
+  return g_pHDInstance ? g_pHDInstance->status : DISK_STATUS_OFF;
+}
+
+void HD_ResetStatus() {
+  if (g_pHDInstance) g_pHDInstance->status = DISK_STATUS_OFF;
+}
+
+static void GetImageTitle(const char* imageFileName, HarddiskDrive_t* pHardDrive)
+{
   char imagetitle[128];
   const char* startpos = imageFileName;
 
-  if (strrchr(startpos, FILE_SEPARATOR)) {
-    startpos = strrchr(startpos, FILE_SEPARATOR) + 1;
+  const char* last_sep = strrchr(startpos, FILE_SEPARATOR);
+  if (last_sep) {
+    startpos = last_sep + 1;
   }
   Util_SafeStrCpy(imagetitle, startpos, 127);
   imagetitle[127] = 0;
@@ -97,9 +157,7 @@ static void GetImageTitle(const char* imageFileName, PHDD pHardDrive) {
   Util_SafeStrCpy(pHardDrive->hd_imagename, imagetitle, 15);
 }
 
-static void NotifyInvalidImage(char* filename) {
-  printf("HDD: Could not load %s\n", filename);
-}
+static void NotifyInvalidImage(const char *filename) { printf("HDD: Could not load %s\n", filename); }
 
 static auto Util_GetFileSize(FILE* f) -> size_t {
   long current = ftell(f);
@@ -109,49 +167,53 @@ static auto Util_GetFileSize(FILE* f) -> size_t {
   return size;
 }
 
-static void HD_CleanupDrive(int nDrive) {
-  g_HardDrive[nDrive].hd_file.reset();
-  g_HardDrive[nDrive].hd_imageloaded = false;
-  g_HardDrive[nDrive].hd_imagename[0] = 0;
-  g_HardDrive[nDrive].hd_fullname[0] = 0;
+static void HD_CleanupDrive(HarddiskDrive_t* pDrive)
+{
+  pDrive->hd_file.reset();
+  pDrive->hd_imageloaded = false;
+  pDrive->hd_imagename[0] = 0;
+  pDrive->hd_fullname[0] = 0;
 }
 
-static auto HD_Load_Image(int nDrive, const char* filename) -> bool {
-  g_HardDrive[nDrive].hd_file.reset(fopen(filename, "r+b"));
-  g_HardDrive[nDrive].hd_imageloaded = g_HardDrive[nDrive].hd_file != nullptr;
-  return g_HardDrive[nDrive].hd_imageloaded;
+static auto HD_Load_Image(HarddiskDrive_t* pDrive, const char *filename) -> bool
+{
+  pDrive->hd_file.reset(fopen(filename, "r+b"));
+  pDrive->hd_imageloaded = pDrive->hd_file != nullptr;
+  return pDrive->hd_imageloaded;
 }
 
-static auto HD_IO_EMUL(void* instance, uint16_t pc, uint16_t addr,
-                       uint8_t bWrite, uint8_t d, uint32_t nCyclesLeft)
-    -> uint8_t;
-
-#include "core/Peripheral.h"
-
-// NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables)
-static HostInterface_t* g_pHDHost = nullptr;
+static auto HD_IO_EMUL(void* instance, uint16_t pc, uint16_t addr, uint8_t bWrite, uint8_t d, uint32_t nCyclesLeft) -> uint8_t;
 
 static auto HD_ABI_Init(int slot, HostInterface_t* host) -> void* {
-  g_pHDHost = host;
-  g_uSlot = slot;
+  auto* hp = new HarddiskPeripheral_t();
+  hp->host = host;
+  hp->slot = static_cast<uint32_t>(slot);
+  hp->enabled = true; // Default to enabled for now to maintain existing behavior
 
-  if (g_bHD_Enabled) {
+  HarddiskLoader_Init();
+
+  if (hp->enabled) {
     uint8_t slot_rom[256];
     memcpy(slot_rom, Hddrvr_dat, 256);
     host->RegisterCxROM(slot, slot_rom);
-    g_bHD_RomLoaded = true;
+    hp->rom_loaded = true;
   }
 
   host->RegisterIO(slot, HD_IO_EMUL, HD_IO_EMUL, nullptr, nullptr);
 
-  return reinterpret_cast<void*>(1);  // Dummy instance
+  g_pHDInstance = hp;
+  return hp;
 }
 
 static void HD_ABI_Reset(void* instance) { (void)instance; }
 
 static void HD_ABI_Shutdown(void* instance) {
-  (void)instance;
-  HD_Cleanup();
+  if (!instance) return;
+  auto* hp = static_cast<HarddiskPeripheral_t*>(instance);
+  for (int i = 0; i < 2; i++) HD_CleanupDrive(&hp->drives[i]);
+  HarddiskLoader_Shutdown();
+  if (g_pHDInstance == hp) g_pHDInstance = nullptr;
+  delete hp;
 }
 
 Peripheral_t g_harddisk_peripheral = {
@@ -176,37 +238,46 @@ extern "C" void Register_Harddisk() {
 #ifdef BUILD_SHARED_PERIPHERAL
 EXPORT_PERIPHERAL(g_harddisk_peripheral)
 #endif
-// NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
 
-auto HD_CardIsEnabled() -> bool { return g_bHD_RomLoaded && g_bHD_Enabled; }
+auto HD_CardIsEnabled() -> bool {
+  return g_pHDInstance && g_pHDInstance->rom_loaded && g_pHDInstance->enabled;
+}
 
 void HD_SetEnabled(bool bEnabled) {
-  if (g_bHD_Enabled == bEnabled) return;
-  g_bHD_Enabled = bEnabled;
-  if (!g_pHDHost) return;
-  if (g_bHD_Enabled) {
+  if (!g_pHDInstance) return;
+  if (g_pHDInstance->enabled == bEnabled) return;
+  g_pHDInstance->enabled = bEnabled;
+  if (!g_pHDInstance->host) return;
+
+  if (g_pHDInstance->enabled) {
     uint8_t slot_rom[256];
     memcpy(slot_rom, Hddrvr_dat, 256);
-    g_pHDHost->RegisterCxROM(g_uSlot, slot_rom);
-    g_bHD_RomLoaded = true;
+    g_pHDInstance->host->RegisterCxROM(static_cast<int>(g_pHDInstance->slot), slot_rom);
+    g_pHDInstance->rom_loaded = true;
   } else {
     uint8_t empty_rom[256] = {0};
-    g_pHDHost->RegisterCxROM(g_uSlot, empty_rom);
-    g_bHD_RomLoaded = false;
+    g_pHDInstance->host->RegisterCxROM(static_cast<int>(g_pHDInstance->slot), empty_rom);
+    g_pHDInstance->rom_loaded = false;
   }
 }
 
 auto HD_GetFullName(int nDrive) -> const char* {
-  return g_HardDrive[nDrive].hd_fullname;
+  if (!g_pHDInstance || nDrive < 0 || nDrive >= 2) return "";
+  return g_pHDInstance->drives[nDrive].hd_fullname;
 }
 
 void HD_Cleanup() {
-  for (int i = 0; i < 2; i++) HD_CleanupDrive(i);
+  if (g_pHDInstance) {
+    for (int i = 0; i < 2; i++) HD_CleanupDrive(&g_pHDInstance->drives[i]);
+  }
 }
 
 void HD_Eject(const int iDrive) {
-  if (g_HardDrive[iDrive].hd_imageloaded) {
-    HD_CleanupDrive(iDrive);
+  if (!g_pHDInstance || iDrive < 0 || iDrive >= 2) return;
+  HarddiskDrive_t* pDrive = &g_pHDInstance->drives[iDrive];
+
+  if (pDrive->hd_imageloaded) {
+    HD_CleanupDrive(pDrive);
     if (iDrive == 0) {
       Configuration::Instance().SetString("Preferences", "Harddisk Image 1",
                                           "");
@@ -219,13 +290,17 @@ void HD_Eject(const int iDrive) {
 }
 
 auto HD_InsertDisk(int nDrive, const char* imageFileName) -> bool {
+  if (!g_pHDInstance || nDrive < 0 || nDrive >= 2) return false;
   if (!imageFileName || *imageFileName == 0x00) return false;
-  if (g_HardDrive[nDrive].hd_imageloaded) HD_CleanupDrive(nDrive);
-  bool result = HD_Load_Image(nDrive, imageFileName);
+
+  HarddiskDrive_t* pDrive = &g_pHDInstance->drives[nDrive];
+  if (pDrive->hd_imageloaded) HD_CleanupDrive(pDrive);
+
+  bool result = HD_Load_Image(pDrive, imageFileName);
   if (result) {
-    GetImageTitle(imageFileName, &g_HardDrive[nDrive]);
+    GetImageTitle(imageFileName, pDrive);
   } else {
-    NotifyInvalidImage(const_cast<char*>(imageFileName));
+    NotifyInvalidImage(imageFileName);
   }
   return result;
 }
@@ -236,19 +311,21 @@ auto HD_InsertDisk2(int nDrive, const char* pszFilename) -> bool {
 
 enum { DEVICE_OK = 0x00, DEVICE_UNKNOWN_ERROR = 0x03, DEVICE_IO_ERROR = 0x08 };
 
-static auto HD_IO_EMUL(void* instance, uint16_t pc, uint16_t addr,
-                       uint8_t bWrite, uint8_t d, uint32_t nCyclesLeft)
-    -> uint8_t {
-  (void)instance;
+static auto HD_IO_EMUL(void* instance, uint16_t pc, uint16_t addr, uint8_t bWrite, uint8_t d, uint32_t nCyclesLeft) -> uint8_t {
+  if (!instance) return DEVICE_UNKNOWN_ERROR;
+  auto* hp = static_cast<HarddiskPeripheral_t*>(instance);
+
   uint8_t r = DEVICE_OK;
   addr &= 0xFF;
-  if (!HD_CardIsEnabled()) return r;
-  PHDD pHDD = &g_HardDrive[g_nHD_UnitNum >> 7];
-  if (bWrite == 0) {  // read
+  if (!hp->rom_loaded || !hp->enabled) return IO_Null(pc, addr, bWrite, d, nCyclesLeft);
+
+  HarddiskDrive_t* pHDD = &hp->drives[hp->unit_num >> 7];
+
+  if (bWrite == 0) { // read
     switch (addr) {
       case 0xF0: {
         if (pHDD->hd_imageloaded) {
-          switch (g_nHD_Command) {
+          switch (hp->command) {
             default:
             case 0x00:  // status
               if (Util_GetFileSize(pHDD->hd_file.get()) == 0) {
@@ -258,7 +335,7 @@ static auto HD_IO_EMUL(void* instance, uint16_t pc, uint16_t addr,
               break;
             case 0x01:  // read
             {
-              HDDStatus = DISK_STATUS_READ;
+              hp->status = DISK_STATUS_READ;
               size_t br = Util_GetFileSize(pHDD->hd_file.get());
               if (static_cast<size_t>(pHDD->hd_diskblock * 512) <= br) {
                 fseek(pHDD->hd_file.get(),
@@ -278,7 +355,7 @@ static auto HD_IO_EMUL(void* instance, uint16_t pc, uint16_t addr,
             } break;
             case 0x02:  // write
             {
-              HDDStatus = DISK_STATUS_WRITE;
+              hp->status = DISK_STATUS_WRITE;
               size_t bw = Util_GetFileSize(pHDD->hd_file.get());
               if (static_cast<size_t>(pHDD->hd_diskblock * 512) <= bw) {
                 memmove(pHDD->hd_buf, mem + pHDD->hd_memblock, 512);
@@ -312,67 +389,37 @@ static auto HD_IO_EMUL(void* instance, uint16_t pc, uint16_t addr,
                   }
                 }
               }
-            } break;
-            case 0x03:  // format
-              HDDStatus = DISK_STATUS_WRITE;
+            }
+              break;
+            case 0x03: //format
+              hp->status = DISK_STATUS_WRITE;
               break;
           }
         } else {
-          HDDStatus = DISK_STATUS_OFF;
+          hp->status = DISK_STATUS_OFF;
           pHDD->hd_error = 1;
           r = DEVICE_UNKNOWN_ERROR;
         }
       } break;
-      case 0xF1:
-        r = pHDD->hd_error;
-        break;
-      case 0xF2:
-        r = g_nHD_Command;
-        break;
-      case 0xF3:
-        r = g_nHD_UnitNum;
-        break;
-      case 0xF4:
-        r = static_cast<uint8_t>(pHDD->hd_memblock & 0x00FF);
-        break;
-      case 0xF5:
-        r = static_cast<uint8_t>((pHDD->hd_memblock & 0xFF00) >> 8);
-        break;
-      case 0xF6:
-        r = static_cast<uint8_t>(pHDD->hd_diskblock & 0x00FF);
-        break;
-      case 0xF7:
-        r = static_cast<uint8_t>((pHDD->hd_diskblock & 0xFF00) >> 8);
-        break;
-      case 0xF8:
-        r = pHDD->hd_buf[pHDD->hd_buf_ptr];
-        pHDD->hd_buf_ptr++;
-        break;
-      default:
-        return IO_Null(pc, addr, bWrite, d, nCyclesLeft);
+      case 0xF1: r = pHDD->hd_error; break;
+      case 0xF2: r = hp->command; break;
+      case 0xF3: r = hp->unit_num; break;
+      case 0xF4: r = static_cast<uint8_t>(pHDD->hd_memblock & 0x00FF); break;
+      case 0xF5: r = static_cast<uint8_t>((pHDD->hd_memblock & 0xFF00) >> 8); break;
+      case 0xF6: r = static_cast<uint8_t>(pHDD->hd_diskblock & 0x00FF); break;
+      case 0xF7: r = static_cast<uint8_t>((pHDD->hd_diskblock & 0xFF00) >> 8); break;
+      case 0xF8: r = pHDD->hd_buf[pHDD->hd_buf_ptr]; pHDD->hd_buf_ptr++; break;
+      default: return IO_Null(pc, addr, bWrite, d, nCyclesLeft);
     }
   } else {  // write
     switch (addr) {
-      case 0xF2:
-        g_nHD_Command = d;
-        break;
-      case 0xF3:
-        g_nHD_UnitNum = d;
-        break;
-      case 0xF4:
-        pHDD->hd_memblock = (pHDD->hd_memblock & 0xFF00) | d;
-        break;
-      case 0xF5:
-        pHDD->hd_memblock = (pHDD->hd_memblock & 0x00FF) | (d << 8);
-        break;
-      case 0xF6:
-        pHDD->hd_diskblock = (pHDD->hd_diskblock & 0xFF00) | d;
-        break;
-      case 0xF7:
-        pHDD->hd_diskblock = (pHDD->hd_diskblock & 0x00FF) | (d << 8);
-        break;
-      default:
-        return IO_Null(pc, addr, bWrite, d, nCyclesLeft);
+      case 0xF2: hp->command = d; break;
+      case 0xF3: hp->unit_num = d; break;
+      case 0xF4: pHDD->hd_memblock = (pHDD->hd_memblock & 0xFF00) | d; break;
+      case 0xF5: pHDD->hd_memblock = (pHDD->hd_memblock & 0x00FF) | (d << 8); break;
+      case 0xF6: pHDD->hd_diskblock = (pHDD->hd_diskblock & 0xFF00) | d; break;
+      case 0xF7: pHDD->hd_diskblock = (pHDD->hd_diskblock & 0x00FF) | (d << 8); break;
+      default: return IO_Null(pc, addr, bWrite, d, nCyclesLeft);
     }
   }
   FrameRefreshStatus(DRAW_LEDS);
