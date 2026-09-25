fea48510304738fdbe7c20c34c6b7e2abbd51e58
David Baucum <david@baucum.me>
2026-09-25 01:01:15 -0400

    Modernize types in Apple2Types.h and remove dead constants

--- src/apple2/Apple2Types.h
+++ src/apple2/Apple2Types.h
@@ -8,20 +8,13 @@ constexpr double CLOCK_6502_NTSC =
     ((M14 * 65.0) / 912.0);                   // 65 cycles per 912 14M clocks
 constexpr double CLOCK_6502_PAL = 1015625.0;  // PAL 6502 clock (1.015625 MHz)
 constexpr double CLOCK_6502 = CLOCK_6502_NTSC;
-constexpr double CLK_Z80 = (CLOCK_6502 * 2);  // Z-80 clock rate is 2.041MHz
-
-constexpr uint32_t uCyclesPerLine = 65;  // 25 cycles HBL & 40 cycles HBL
-constexpr uint32_t uVisibleLinesPerFrame = 64 * 3;  // 192 visible lines
-constexpr uint32_t uLinesPerFrame = 262;            // 192 visible + 70 VBL
 
 constexpr int NUM_SLOTS = 8;
 constexpr uint32_t APPLE2_6502_MEM_END = 0xFFFF;
-constexpr uint32_t APPLE2_6502_MEM_LEN = APPLE2_6502_MEM_END + 1;
 
 constexpr uint8_t APPLE2E_MASK = 0x10;
-constexpr uint8_t APPLE2C_MASK = 0x20;
 
-enum eApple2Type {
+enum Apple2Type_t {
   A2TYPE_APPLE2 = 0,
   A2TYPE_APPLE2PLUS,
   A2TYPE_APPLE2JPLUS,
@@ -34,8 +27,9 @@ enum eApple2Type {
   A2TYPE_CLONE_TK3000E,
   A2TYPE_MAX
 };
+using eApple2Type = Apple2Type_t;
 
-enum eApple2Language {
+enum Apple2Language_t {
   A2LANG_US = 1,
   A2LANG_UK,
   A2LANG_FR,
@@ -43,12 +37,10 @@ enum eApple2Language {
   A2LANG_JP_ROMAN,
   A2LANG_JP_KANA
 };
+using eApple2Language = Apple2Language_t;
 
-extern eApple2Type g_apple2_type;
-extern eApple2Language g_language;
+extern Apple2Type_t g_apple2_type;
+extern Apple2Language_t g_language;
 
-inline auto IS_APPLE2() -> bool {
-  return (g_apple2_type & (APPLE2E_MASK | APPLE2C_MASK)) == 0;
-}
-inline auto IS_APPLE2E() -> bool { return (g_apple2_type & APPLE2E_MASK) != 0; }
-inline auto IS_APPLE2C() -> bool { return (g_apple2_type & APPLE2C_MASK) != 0; }
+inline auto is_apple2() -> bool { return (g_apple2_type & APPLE2E_MASK) == 0; }
+inline auto IS_APPLE2() -> bool { return is_apple2(); }
